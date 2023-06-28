8fa1f279e9e7d1cd941bcd8e4372494f9ed09bdf
Kirill Markov <isikir668@gmail.com>
2023-06-28 19:47:38 +0300

    CI

---
+++ src/test/java/AppCardDeliveryTest.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AppCardDeliveryTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    public String generate() {
+        LocalDate time = LocalDate.now();
+        time = time.plusDays(4);
+        String date = time.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    @Test
+    void cardShouldBeDelivered() {
+
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(generate());
+        form.$("[data-test-id=name] input").setValue("Сергей Марочкин");
+        form.$("[data-test-id=phone] input").setValue("+70000000001");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
+
+
